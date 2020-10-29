749746f43ea1e76209514966fa4c90c2932e8d52
Murad <muradkarammaev@gmail.com>
2020-10-29 17:27:25 +0300

    decide which Hamming code is appropriate for which k

---
+++ optimal-hamming-code.c
@@ -0,0 +1,45 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <qc.h>
+
+static size_t powu(size_t x, size_t n) {
+    size_t ret = 1;
+    for (size_t i = 0; i < n; ++i) {
+        ret *= x;
+    }
+    return ret;
+}
+
+static void hamming_parameters(size_t s, size_t *n, size_t *k) {
+    *n = powu(2, s) - 1;
+    *k = *n - s;
+}
+
+int main(int argc, char* argv[]) {
+    size_t target_k;
+    qc_args* args = qc_args_new();
+    qc_args_brief(args,
+            "Find the most optimal Hamming code with specified information\n"
+            "vector bit length, also generate appropriate matrices G and H\n");
+    qc_args_unsigned_default(args, "k", 4, &target_k, "information vector bits");
+    char* err;
+    if (!qc_args_parse(args, argc, argv, &err)) {
+        fprintf(stderr, "Failed to parse arguments: %s\n", err);
+        free(err);
+        qc_args_free(args);
+        exit(EXIT_FAILURE);
+    }
+    if (target_k == 0) {
+        fputs("k is expected to be positive\n", stderr);
+        exit(EXIT_FAILURE);
+    }
+    size_t s = 0, n = 0, k = 0;
+    while (k < target_k) {
+        ++s;
+        hamming_parameters(s, &n, &k);
+    }
+    printf("The best appropriate canonical Hamming code is (%zu,%zu,3)\n", n, k);
+    printf("The best appropriate Hamming code is (%zu,%zu,3)\n", n-k+target_k, target_k);
+    qc_args_free(args);
+}
