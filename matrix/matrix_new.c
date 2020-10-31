05edb0046f770b5a4404b95105b988667e084430
Murad <muradkarammaev@gmail.com>
2020-10-31 17:40:17 +0300

    add size assertion to matrix_new()

--- matrix/matrix_new.c
+++ matrix/matrix_new.c
@@ -1,9 +1,11 @@
 #include <stddef.h>
 #include <string.h>
+#include <assert.h>
 #include <qc.h>
 #include "matrix.h"
 
 bit** matrix_new(size_t m, size_t n) {
+    assert(m != 0 && n != 0);
     size_t* mem = emalloc(m * sizeof(bit*) + 2 * sizeof(size_t));
     mem[0] = m;
     mem[1] = n;
